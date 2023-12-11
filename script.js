b91b4a381f70ed4ab8068977368a0590a7bf17e3
nthnlgmz <143614589+nthnlgmz@users.noreply.github.com>
2023-12-11 12:19:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -12,24 +12,5 @@
         }
       });
     });
- document.addEventListener('DOMContentLoaded', function () {
-      const burger = document.querySelector('.navbar-burger');
-      const drawer = document.querySelector('.drawer');
-      
-      burger.addEventListener('click', function () {
-        drawer.classList.toggle('open');
-      });
-      
-      document.addEventListener('click', function (event) {
-        if (drawer.classList.contains('open') && !drawer.contains(event.target) && !burger.contains(event.target)) {
-          drawer.classList.remove('open');
-        }
-      });
-    });
-
-if (window.location.href.indexOf("https://nthnlgmz.github.io/Reflection-Journal/") === -1) {
-    // Redirect to a custom 404 error page
-    window.location.href = "https://dione.batstate-u.edu.ph/student/#/";
-}
 
 
